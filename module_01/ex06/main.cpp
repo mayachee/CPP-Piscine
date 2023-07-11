0c83feb48b522d34d74f604e87409d7b891cf6af
siiine-764 <yassinepro764@gmail.com>
2023-07-11 23:18:04 +0200

    ex06 module 01

---
+++ module_01/ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Karen.hpp"
+
+int main() {
+    Karen karen;
+
+    // karen.KeranFilter("DEBUG");
+    karen.KeranFilter("INFO");
+    // karen.KeranFilter("I am not sure how tired I am today...");
+    // karen.KeranFilter("ERROR");
+
+    return 0;
+}
