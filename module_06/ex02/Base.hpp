dc24e237eaa923c1cdd307399a4096fa400f5b1e
siiine-764 <yassinepro764@gmail.com>
2024-05-22 21:05:32 +0100

    ff

--- module_06/ex02/Base.hpp
+++ module_06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: mayache- <mayache-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 19:09:23 by mayache-          #+#    #+#             */
-/*   Updated: 2024/05/21 22:25:33 by mayache-         ###   ########.fr       */
+/*   Updated: 2024/05/22 20:51:04 by mayache-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,4 @@ class Base {
         virtual ~Base() = 0; 
 };
 
-// Base* generate();
-// void identify(Base* p);
-// void identify(Base& p);
 #endif
