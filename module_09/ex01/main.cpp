0ec2b66f1ca9a44b4e46dcbd4e5c7875f6d3dd04
siiine-764 <yassinepro764@gmail.com>
2024-11-30 14:55:16 +0100

    ex01 module09

---
+++ module_09/ex01/main.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayache- <mayache-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 14:40:06 by mayache-          #+#    #+#             */
+/*   Updated: 2024/11/30 14:51:25 by mayache-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <stack>
+// #include <string>
+#include <sstream>
+
+bool is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int evaluate_rpn(const std::string& expression) {
+    std::stack<int> operands;
+
+    std::istringstream iss(expression);
+    std::string token;
+
+    while (iss >> token) {
+        if (isdigit(token[0])) {
+            operands.push(std::stoi(token));
+        } else if (is_operator(token[0])) {
+            if (operands.size() < 2) {
+                std::cerr << "Error: Invalid expression\n";
+                return -1;
+            }
+
+            int operand2 = operands.top();
+            operands.pop();
+            int operand1 = operands.top();
+            operands.pop();
+
+            switch (token[0]) {
+                case '+':
+                    operands.push(operand1 + operand2);
+                    break;
+                case '-':
+                    operands.push(operand1 - operand2);
+                    break;
+                case '*':
+                    operands.push(operand1 * operand2);
+                    break;
+                case '/':
+                    if (operand2 == 0) {
+                        std::cerr << "Error: Division by zero\n";
+                        return -1;
+                    }
+                    operands.push(operand1 / operand2);
+                    break;
+            }
+        } else {
+            std::cerr << "Error: Invalid token\n";
+            return -1;
+        }
+    }
+
+    if (operands.size() != 1) {
+        std::cerr << "Error: Invalid expression\n";
+        return -1;
+    }
+
+    return operands.top();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <RPN expression>\n";
+        return 1;
+    }
+
+    std::string expression = argv[1];
+    int result = evaluate_rpn(expression);
+
+    if (result != -1) {
+        std::cout << result << std::endl;
+    }
+
+    return 0;
+}
